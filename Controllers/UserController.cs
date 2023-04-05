327270e015ba6bf3b67f9ece08787e7cc8076024
SamuelLaguna <samanater11@gmail.com>
2023-04-05 15:00:08 -0700

    User Services code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendfoodapi.Models.DTO;
+using backendfoodapi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backendfoodapi.Controllers;
@@ -10,7 +12,15 @@ namespace backendfoodapi.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
-    
+    private readonly UserService _data;
+    public UserController(UserService dataFromService){
+        _data = dataFromService;
+        
+    }
+
+    public bool AddUser(CreateAccountDTO UserToAdd)
+    {
+        return _data.AddUser(UserToAdd);
+    }
     
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,11 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendfoodapi.Models;
+using backendfoodapi.Models.DTO;
+using backendfoodapi.Services.Context;
+using System.Security.Cryptography;
 
 namespace backendfoodapi.Services
 {
     public class UserService
     {
-        
+
+        private readonly DataContext _context;
+        public UserService(DataContext context){
+            _context = context;
+        }
+
+        public bool DoesUserExists(string? Username){
+            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) !=null;
+
+        }
+
+
+
+
+
+
+        public bool AddUser(CreateAccountDTO UserToAdd){
+            bool result = false;
+            if(!DoesUserExists(UserToAdd.Username))
+            {
+                UserModel newUser = new UserModel();
+                result = true;
+            }
+
+            return result;
+        }
+
+
+        public PasswordDTO HashPassword(string password)
+        {
+            PasswordDTO newHashedPasword = new PasswordDTO();
+
+            byte[] SaltByte = new byte[64];
+            var provider = new RNGCryptoServiceProvider();
+            provider.GetNonZeroBytes(SaltByte);
+
+
+            var Salt = Convert.ToBase64String(SaltByte);
+
+        }
+       
     }
 }
