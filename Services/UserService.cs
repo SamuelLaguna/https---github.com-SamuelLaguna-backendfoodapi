b82bbb814f3a94ed17682e73cd126a9948b97971
SamuelLaguna <samanater11@gmail.com>
2023-04-07 10:12:52 -0700

    UserService

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -13,12 +13,14 @@ namespace backendfoodapi.Services
     {
 
         private readonly DataContext _context;
-        public UserService(DataContext context){
+        public UserService(DataContext context)
+        {
             _context = context;
         }
 
-        public bool DoesUserExists(string? Username){
-            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) !=null;
+        public bool DoesUserExists(string? Username)
+        {
+            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) != null;
 
         }
 
@@ -27,19 +29,33 @@ namespace backendfoodapi.Services
 
 
 
-        public bool AddUser(CreateAccountDTO UserToAdd){
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
             bool result = false;
-            if(!DoesUserExists(UserToAdd.Username))
+            if (!DoesUserExists(UserToAdd.Username))
             {
                 UserModel newUser = new UserModel();
-                result = true;
+                // Create are hash and salt password
+                var hashPassword = HashPassword(UserToAdd.Password);
+                newUser.Id = UserToAdd.id;
+                newUser.UserName = UserToAdd.Username;
+                newUser.Salt = hashPassword.Salt;
+                newUser.Hash = hashPassword.Hash;
+
+                //Adding new user to our database 
+                _context.Add(newUser);
+
+                //This saves to our database and returns the number of entriees written in our database.
+                // _context.SavedChanges();
+
+                result = _context.SaveChanges() !=0;            
             }
 
             return result;
         }
 
 
-        public PasswordDTO HashPassword(string password)
+        public PasswordDTO HashPassword(string? password)
         {
             PasswordDTO newHashedPasword = new PasswordDTO();
 
@@ -50,7 +66,31 @@ namespace backendfoodapi.Services
 
             var Salt = Convert.ToBase64String(SaltByte);
 
+            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltByte, 10000);
+
+            var Hash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+
+            newHashedPasword.Salt = Salt;
+            newHashedPasword.Hash = Hash;
+
+            return newHashedPasword;
+
+
         }
-       
+
+        public bool VerifyUserPassword(string? Password, string storedHash, string storedSalt)
+        {
+            //Get our existing salt and change it to base 64 string
+            var SaltBytes = Convert.FromBase64String(storedSalt);
+            // We are making the password that the user inputed and using the stored salt
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(Password, SaltBytes, 10000);
+
+            var newHash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+            // Checking if the new hash is the same as the stored hash
+            return newHash == storedHash;
+
+        }
+
+
     }
 }
